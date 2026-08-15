83aa4e22250d02af67d13cdce1611bc086cfba95
JustModo <yashlaxman@gmail.com>
2026-08-15 12:46:33 +0530

    fix: remove undici

--- server/index.js
+++ server/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const { Agent, setGlobalDispatcher } = require("undici");
-
-setGlobalDispatcher(new Agent({ connections: 50 }));
 
 const app = express();
 
